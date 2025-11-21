1aa63a05442ee6aa15a8b1e76c3e42831c7b33ce
diivvuu <divyanshubawa9@gmail.com>
2025-11-21 21:09:23 +0530

    deployment fix

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,9 @@
+// src/api/index.ts
 import serverless from 'serverless-http';
 import app from '../server';
+
+export default serverless(app);
 
 export const config = {
   runtime: 'nodejs22.x',
 };
-
-export default serverless(app);
